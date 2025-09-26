3ce78fa273f4ec5b38b17be7ea834ea9dde8eb60
Seth Aguilar <sethcaguilar@gmail.com>
2025-09-26 15:53:49 -0700

    Name change for workoutCard + workoutActivity dataclass

---
+++ lib/widgets/workoutCard.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../models/user.dart';
+
+class WorkoutCard extends StatelessWidget {
+  final WorkoutActivity activity
+  const WorkoutCard({super.key, required this.activity})
+}

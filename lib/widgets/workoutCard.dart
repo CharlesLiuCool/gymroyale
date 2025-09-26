699129369373e201beeb08eea476058d8904db6c
Seth Aguilar <sethcaguilar@gmail.com>
2025-09-26 16:07:49 -0700

    some workout widget skelton code

--- lib/widgets/workoutCard.dart
+++ lib/widgets/workoutCard.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import '../models/user.dart';
+import '../models/workoutActivity.dart';
 
 class WorkoutCard extends StatelessWidget {
-  final WorkoutActivity activity
-  const WorkoutCard({super.key, required this.activity})
+  final WorkoutActivity activity;
+  const WorkoutCard({super.key, required this.activity});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }

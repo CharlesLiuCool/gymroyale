b8d4d202a8b558d5f2d6944e0305487dd4920526
seth-aguilar <sethcaguilar@gmail.com>
2025-10-23 22:03:19 -0700

    new feed page with workout cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gymroyale/models/workoutActivity.dart';
+import 'package:gymroyale/widgets/workoutCard.dart';
 import 'firebase_options.dart';
 import 'repositories/leaderboard_repository.dart';
 import 'widgets/leaderboard.dart';
@@ -20,6 +22,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final repo = LeaderboardRepository();
 
+    final activity = WorkoutActivity(
+      id: 'a1', 
+      title: 'Evening lift', 
+      activityType: ActivityType.lift, 
+      startedAt: DateTime.now().subtract(const Duration(hours: 3, minutes: 12)), 
+      movingTime: const Duration(minutes:42, seconds: 10), 
+      likeCount: 12, 
+      commentsCount: 3
+    );
+
+    final activity2 = WorkoutActivity(
+      id: 'a2', 
+      title: 'Afternoon Treadmill', 
+      activityType: ActivityType.cardio, 
+      startedAt: DateTime.now().subtract(const Duration(hours: 2, minutes: 30)), 
+      movingTime: const Duration(minutes: 15, seconds: 30), 
+      likeCount: 30, 
+      commentsCount: 5
+    );
+
+    final activity3 = WorkoutActivity(
+      id: 'a3', 
+      title: 'Morning workout + run', 
+      activityType: ActivityType.blend, 
+      startedAt: DateTime.now().subtract(const Duration(hours: 4, minutes: 45)), 
+      movingTime: const Duration(hours: 1, minutes: 15, seconds: 23), 
+      likeCount: 60, 
+      commentsCount: 15
+    );
+
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
@@ -27,8 +60,15 @@ class MyApp extends StatelessWidget {
 
       home: Scaffold(
         appBar: AppBar(title: const Text('Leaderboard')),
-        body: Leaderboard(repo: repo),
-        // FUTURE: Update UserId to be unique per person (perhaps set it as username?)
+        body: ListView(
+          children: [
+          Leaderboard(repo: repo),
+          // FUTURE: Update UserId to be unique per person (perhaps set it as username?)
+          WorkoutCard(activity: activity),
+          WorkoutCard(activity: activity2),
+          WorkoutCard(activity: activity3)
+          ],
+        ),
         floatingActionButton: GymCheckInButton(
           userId: 'UserIdHere',
           repo: repo,

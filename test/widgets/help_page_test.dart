5061416f0432ccb5d542c53e08b8710402d07dcd
Seth Aguilar <sethcaguilar@gmail.com>
2025-11-30 16:46:21 -0800

    updated help page test to reflect new dir

--- test/widgets/help_page_test.dart
+++ test/widgets/help_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gymroyale/pages/main/help_page.dart';
+import 'package:gymroyale/pages/main/settings/help_page.dart';
 
 void main() {
   testWidgets('HelpPage shows FAQ items', (tester) async {
